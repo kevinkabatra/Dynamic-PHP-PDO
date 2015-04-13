b44feada9f5393be0bd71a23aea489385d7666e1
Kevin Kabatra <kevinkabatra@gmail.com>
2015-04-13 11:52:54 -0400

    Updated file.
    Major Updates:
        1) Changed insertIntoDatabase() function to insertOneIntoDatabase() function, name changed to clarify what should be expected. 
        2) Added unset() function call to each function, to destroy unused variables.
        3) Added selectOneFromDatabase() function.
        4) Changed selectFromDatabase() function to selectAllFromDatabase() function, name changed to clarify what should be expected.
        5) Added updateOneFromDatabase() function.
    Minor Updates:
        1) Corrected  example code for insertOneIntoDatabase() function.
        2) Moved include_once call to happen only at start of code, removed duplicate entries of include_once.
        3) Changed wording in error handling for handlePdoExceptions() function.

--- php_data_objects.php
+++ php_data_objects.php
@@ -37,6 +37,8 @@
  *     .html#follow-field-naming-conventions
  */
   
+  include_once 'get_post_parameter_fields_values.php';
+  
     /**
      * Creates a new connection to a MySQL database
      * <p>
@@ -94,14 +96,13 @@
     }
     
     /**
-     * Used to insert records from a MySQL database table.
+     * Used to insert one record into a MySQL database table.
      * <p>
      * Example code:
      * try {        
      *     $connection = connectDatabase('databaseName', 'serverName'
      *             , 'username', 'password');
-     *     //Send subjects, salt to bizTwoBusiness_subjects
-     *     $tableName = "subjects";
+     *     $tableName = "tableName";
      *     //Fields and Values to send to database
      *     $postParameter = array(
      *             'key1' => value1,
@@ -120,8 +121,7 @@
      * @param type $mTableName
      * @param type $mPostParameter
      */
-    function insertIntoDatabase($mConnection, $mTableName, $mPostParameter) {
-        
+    function insertOneIntoDatabase($mConnection, $mTableName, $mPostParameter) {        
         try {
             //prepare sql
             $mStatement = $mConnection->prepare("INSERT INTO $mTableName (" 
@@ -135,9 +135,16 @@
             }            
             
             //row insertion
-            $mStatement->execute();        
+            $mStatement->execute();
+            
+            //unset() destroys the specified variables.
+            unset($mKey);
+            unset($mValue);
+            unset($mConnection);
+            unset($mTableName);
+            unset($mPostParameter);
         } catch(PDOException $mPdoException) {
-            handlePdoExceptions($mPdoException->getMessage());
+            return handlePdoExceptions($mPdoException->getMessage());
         } finally {
             unset($mStatement);
         }                       
@@ -166,7 +173,6 @@
      * @param type $mPostParameter
      */
     function deleteFromDatabase($mConnection, $mTableName, $mPostParameter) {
-        
         try {
             //prepare sql
             $mStatement = $mConnection->prepare("DELETE FROM $mTableName WHERE"
@@ -178,25 +184,87 @@
             foreach($mPostParameter as $mKey => $mValue) {
                 $mStatement->bindValue(':' . $mKey, $mValue);
             }            
+                        
+            $mStatement->execute();
             
-            //row insertion
-            $mStatement->execute();        
+            //unset() destroys the specified variables.
+            unset($mKey);
+            unset($mValue);
+            unset($mConnection);
+            unset($mTableName);
+            unset($mPostParameter);            
         } catch(PDOException $mPdoException) {
             handlePdoExceptions($mPdoException->getMessage());
         } finally {
             unset($mStatement);
-        }        
-    }
-
+        }
+    }        
+    
     /**
-     * Used to select records from a MySQL database table.
+     * Selects one record from a MySQL database table.
+     * <p>
+     * Example code:
+     *     try {
+     *         $connection = connectDatabase('databaseName'
+     *                 , 'serverName', 'username', 'password');
+     *         $tableName = 'tableName';
+     *         $postParameter = array(
+     *             'id' => 1,
+     *         );
+     *         $selected = selectOneFromDatabase($connection, $tableName
+     *                 , $postParameter);
+     *         echo var_dump($selected);
+     *      } catch (PDOException $pdoException) {
+     *          handlePdoExceptions($pdoException->getMessage());
+     *      } finally {
+     *          unset($connection);
+     *      }
+     * <p>
+     * @param type $mConnection
+     * @param type $mTableName
+     * @param type $mPostParameter
+     * @return type
+     */
+    function selectOneFromDatabase($mConnection, $mTableName, $mPostParameter) {
+        try {
+            //prepare sql        
+            $mStatement = $mConnection->prepare('SELECT * FROM ' . $mTableName  
+                    . ' WHERE ' . getFields($mPostParameter) . ' = ' 
+                    . getBindFields($mPostParameter));
+
+            //bind parameters
+            $mKey = $mValue = null;
+            foreach($mPostParameter as $mKey => $mValue) {
+                    $mStatement->bindValue(':' . $mKey, $mValue);
+            }
+            
+            $mStatement->execute();
+            $mStatement->setFetchMode(PDO::FETCH_ASSOC);
+
+            //unset() destroys the specified variables.
+            unset($mKey);
+            unset($mValue);
+            unset($mConnection);
+            unset($mTableName);
+            unset($mPostParameter);
+            
+            return var_dump($mStatement->fetch());
+        } catch(PDOException $mPdoException) {
+            handlePdoExceptions($mPdoException->getMessage());
+        } finally {
+            unset($mStatement);
+        }
+    }
+    
+    /**
+     * Used to select all records from a MySQL database table.
      * <p>
      * @param type $mConnection
      * @param type $mTableName
      * @param type $mPostParameter
      * @return boolean
      */
-    function selectFromDatabase($mConnection, $mTableName, $mPostParameter) {
+    function selectAllFromDatabase($mConnection, $mTableName, $mPostParameter) {
         //prepare sql
         $mStatement = $mConnection->prepare("SELECT " 
                 . getFields($mPostParameter) . "FROM $mTableName");
@@ -246,14 +314,39 @@
     }
     
     /**
-     * 
+     * Used to update a record from a MySQL database table.
+     * @param type $mConnection
+     * @param type $mTableName
      * @param type $mPostParameter
-     * @return type
      */
-    function getFields($mPostParameter) {
-        include_once '../database_tools/get_post_parameter_fields_values.php';
-        $mFields = getPostParameterFieldsValues("field", $mPostParameter);        
-        return $mFields;
+    function updateOneFromDatabase($mConnection, $mTableName, $mPostParameter,
+            $mWhereColumn, $mWhereValue) {
+        try {
+            //prepare sql
+            $mStatement = $mConnection->prepare('UPDATE ' . $mTableName 
+                    . 'SET ' . getFields($mPostParameter) . '=' 
+                    . getBindFields($mPostParameter) . 'WHERE ' . $mWhereColumn 
+                    . '=' . $mWhereValue);
+
+            //bind parameters
+            $mKey = $mValue = null;
+            foreach($mPostParameter as $mKey => $mValue) {
+                $mStatement->bindValue(':' . $mKey, $mValue);
+            } 
+
+            $mStatement->execute(); 
+
+            //unset() destroys the specified variables.
+            unset($mKey);
+            unset($mValue);
+            unset($mConnection);
+            unset($mTableName);
+            unset($mPostParameter);
+        } catch(PDOException $mPdoException) {
+            handlePdoExceptions($mPdoException->getMessage());
+        } finally {
+            unset($mStatement);
+        }
     }
     
     /**
@@ -261,8 +354,27 @@
      * @param type $mPostParameter
      * @return type
      */
+    function getFields($mPostParameter) {        
+        $mFields = getPostParameterFieldsValues("field", $mPostParameter);
+        return $mFields;
+    }
+    
+/**
+     * 
+     * @param type $mPostParameter
+     * @return type
+     */
+    function getValues($mPostParameter) {
+        $mValues = getPostParameterFieldsValues("value", $mPostParameter);
+        return $mValues;
+    }    
+    
+    /**
+     * 
+     * @param type $mPostParameter
+     * @return type
+     */
     function getBindFields($mPostParameter) {
-        include_once '../database_tools/get_post_parameter_fields_values.php';
         $mBindFields = getPostParameterBindFields($mPostParameter);
         return $mBindFields;
     }
@@ -272,12 +384,11 @@
      * @param type $mPdoException
      */
     function handlePdoExceptions($mPdoException) {        
-        echo $mPdoException;
         if(strpos($mPdoException, "SQLSTATE[23000]") !== false) {
-            echo "duplicate entry";
+            echo "Error: Duplicate entry.<br>";
             if(strpos($mPdoException, "'subject'") !== false) {
                 //TODO: Provide a link to recover account
-                echo "Error: that username already exists.";
+                echo "Error: that username already exists.<br>";
             } else if(strpos($mPdoException, "'id'") !== false) {
                 //TODO: Provide logging to a developer console
                 //critical error, the database did not automatically auto increment                
@@ -294,4 +405,4 @@
             //You have an error in your SQL syntax
         }
         return $mPdoException;
-    }    
+    }
